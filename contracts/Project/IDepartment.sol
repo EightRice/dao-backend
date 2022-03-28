cb3e71066ffefbe6323d2335c52ccc745d2b963f
Leonhard Horstmeyer <leo.h@hotmail.de>
2022-03-28 20:35:39 +0200

    adjusted the interface to accommodate for the simplified payout function

--- contracts/Project/IDepartment.sol
+++ contracts/Project/IDepartment.sol
@@ -3,6 +3,5 @@ pragma solidity ^0.8.7;
 
 
 interface IInternalProject {
-    function pay() external returns(uint256 totalPaymentValue, uint256 totalRepValue);
-    function withdraw() external;
+    function payout(uint256 shareValue) external;
 }
